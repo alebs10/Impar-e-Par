ed07e49b6d301e10940fc17356fdaecd06238b75
alebs10 <alexandre2002.ab@gmail.com>
2023-03-09 11:24:43 -0300

    Par e ou Impar.

---
+++ ParImpar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+
+            Console.WriteLine("Olá ^_^");
+            Thread.Sleep(3600);
+            Console.Clear();
+
+            Console.WriteLine("Digite um número por favor: ");
+            numero = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(numero % 2 == 0 ? "Seu número é Par" : "Seu número é Impar");
+            Console.WriteLine("Digite qualquer tecla para encerrar...");
+            Console.ReadKey();
+        }
+    }
+}
